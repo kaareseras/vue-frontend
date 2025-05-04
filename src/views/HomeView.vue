b930ff7fa04026ea0aa9ee6fdf3227d469b49ad1
kaareseras <kaare@seras.dk>
2025-05-04 17:01:50 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import Homecards from '@/components/Homecards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
+
+<template>
+
+
+  <!-- <Hero title="Test Title" subtitle="Default Subtitle"/> -->
+  <Hero />
+  <Homecards />
+  <JobListings :limit="3" :showButton="true" />
+
+</template>
